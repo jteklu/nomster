87b8ab634d172b22d6d7e1c569f415a553577109
Jonathan <jteklu1@umbc.edu>
2015-08-05 17:58:00 +0000

    Added will_paginate gem to Gemfile and adjusted places_contoller.rb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,10 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.all
+		#This is the new and shorter form of writing the page code.
+		@places = Place.page(params[:page]).order('created_at DESC')
+		
+		#These are additional ways of writing this code, learn them and practice it.
+		#@places = Place.paginate :page => params[:page], :order => 'created_at DESC'
+		#@places = Place.paginate(:page => params[:page], :order => 'created_at DESC')
 	end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ActiveRecord::Base
+	self.per_page = 10
 end
+
