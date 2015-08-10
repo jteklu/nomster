e19d8b98ca74351c1a7821c5b84af33529a7b3a0
Jonathan <jteklu1@umbc.edu>
2015-08-10 12:36:08 +0000

    Adjusted model, added validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@ class Place < ActiveRecord::Base
 	self.per_page = 10
 	
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => {:minimum => 3}
+	validates :address, :presence => true
+	validates :description, :presence => true, :length => {:maximum => 140, :minimum => 3}
 end
 
