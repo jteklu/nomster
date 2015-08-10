601e32a34db100df842d48190858c77c5ef6c33c
Jonathan <jteklu1@umbc.edu>
2015-08-10 10:34:37 +0000

    Adjusted places_controller.rb and added user validation for editing and updating of user generated records

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
 	def index
 		#This is the new and shorter form of writing the page code.
@@ -25,10 +25,18 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
 	end
 
 	def update
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
+
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
