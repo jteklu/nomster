ba6260a08e9fae3e45a9ed3d0017bd150561175d
Jonathan <jteklu1@umbc.edu>
2015-08-13 18:14:01 +0000

    Adjusted application.yml file for email delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,8 @@
 Nomster::Application.configure do
   config.action_mailer.delivery_method = :smtp
-
+  
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.gmail.com", 
     port: 587,
     domain: "example.com",
     authentication: "plain",
